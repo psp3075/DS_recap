d39f24311c019ab86eef41b4ec1950a6a25ba0a2
Pavan S Patil <psp3075@gmail.com>
2024-01-04 11:49:33 +0530

    queue

---
+++ queue2.js
@@ -0,0 +1,47 @@
+class Queue2 {
+  constructor() {
+    this.items = {};
+    this.front = 0;
+    this.rear = 0;
+  }
+
+  enqueue(element) {
+    this.items[this.rear] = element;
+    this.rear++;
+  }
+
+  dequeue() {
+    const item = this.items[this.front];
+    delete this.items[this.front];
+    this.front++;
+    return item;
+  }
+
+  isEmpty() {
+    return this.rear - this.front === 0;
+  }
+
+  peek() {
+    return this.items[this.front];
+  }
+
+  size() {
+    return this.rear - this.front;
+  }
+
+  print() {
+    console.log(Object.values(this.items));
+  }
+}
+
+const queue = new Queue2();
+console.log(queue.isEmpty());
+
+queue.enqueue(10);
+queue.enqueue(20);
+queue.enqueue(30);
+console.log(queue.size());
+queue.print();
+
+console.log(queue.dequeue());
+console.log(queue.peek());
